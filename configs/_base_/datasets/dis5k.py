72c8ddd245cb0997eb16decbeae02fd251ed3129
kprokofi <kirill.prokofiev@intel.com>
2023-03-17 11:52:08 +0900

    added dis5k

--- configs/_base_/datasets/dis5k.py
+++ configs/_base_/datasets/dis5k.py
@@ -1,5 +1,5 @@
-dataset_type='CityscapesHalf'
-data_root='data/citiscapes_semisl'
+dataset_type='DIS5K'
+data_root='data/DIS5K'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
--- mmseg/datasets/custom.py
+++ mmseg/datasets/custom.py
@@ -542,11 +542,11 @@ class VOCHalf(CustomDataset):
 
 @DATASETS.register_module()
 class CityscapesHalf(CustomDataset):
-    CLASSES = ('background', 'road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
+    CLASSES = ('road', 'sidewalk', 'building', 'wall', 'fence', 'pole',
                'traffic light', 'traffic sign', 'vegetation', 'terrain', 'sky',
                'person', 'rider', 'car', 'truck', 'bus', 'train', 'motorcycle',
                'bicycle')
-    PALETTE = [(0,0,0), [128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
+    PALETTE = [[128, 64, 128], [244, 35, 232], [70, 70, 70], [102, 102, 156],
                [190, 153, 153], [153, 153, 153], [250, 170, 30], [220, 220, 0],
                [107, 142, 35], [152, 251, 152], [70, 130, 180], [220, 20, 60],
                [255, 0, 0], [0, 0, 142], [0, 0, 70], [0, 60, 100],
